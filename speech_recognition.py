1731f8afc72df81bf6a506cb47b060a187b36f80
ChrisCruze <cruzc09@gmail.com>
2021-04-20 10:49:43 -0400

    initial commit with working example of speech recognition

---
+++ speech_recognition.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+harvard = sr.AudioFile('harvard.wav')
+
+
+with harvard as source:
+	audio = r.record(source)
+
+
+type(audio)
+
+r.recognize_google(audio)
